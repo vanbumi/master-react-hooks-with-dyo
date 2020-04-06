539f46cc36f46f5b947f66b33c01bb414db32d61
vanbumi <jsp.dio@gmail.com>
2020-04-06 19:24:36 +0700

    Perbedaan antara keyword Function dan arrow Function done

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
+// function createObject() {
+//     console.log('outmost this', this);
+
+//     return {
+//         arrowFunction: () => {
+//             console.log('arrowFunction this lah', this);
+//         },
+//         functionKeywordFunction: function() {
+//             console.log('functionKeyword this lah', this);
+//         }
+//     }
+// }
+
+// const obj = createObject();
+// console.log('obj', obj);
+
+// obj.arrowFunction();
+// obj.functionKeywordFunction();
+
+// cek hasilnya di console
